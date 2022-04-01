d0ba4e4d8e40207b86e818a2ae0b62c257866069
Vedanth Bora <vedanthbora@gmail.com>
2022-04-01 15:33:02 +0530

    [FEAT]: added shop page

---
+++ src/routes/Shop.js
@@ -0,0 +1,37 @@
+import React from "react";
+import FiltersSideBar from "../components/shop-page/FiltersSideBar";
+import ProductPageCard from "../components/shop-page/ProductPageCard";
+import SortProductsDropdown from "../components/shop-page/SortProductsDropdown";
+
+function Shop() {
+  return (
+    <main
+      style={{ top: "5rem", minHeight: "180vh" }}
+      className="relative w-full justify-end flex"
+    >
+      <FiltersSideBar />
+
+      <div className="products-display-container">
+        <div className="flex w-full justify-end">
+          <SortProductsDropdown />
+        </div>
+
+        <div style={{ gap: "4rem" }} className="grid grid-cols-3">
+          {/* <ProductCard product={{}} />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard /> */}
+        </div>
+
+        <div className="spacer-3rem"></div>
+        <div className="spacer-3rem"></div>
+        <div className="spacer-3rem"></div>
+      </div>
+    </main>
+  );
+}
+
+export default Shop;
