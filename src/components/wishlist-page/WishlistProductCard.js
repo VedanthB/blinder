1fbee6fffbc0c5d18d206a025ce5a19d02931e5f
Vedanth Bora <vedanthbora@gmail.com>
2022-04-06 14:38:35 +0530

    [FEAT]: added delete from wishlist api/ func

--- src/components/wishlist-page/WishlistProductCard.js
+++ src/components/wishlist-page/WishlistProductCard.js
@@ -1,59 +1,122 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../context/providers/AuthProvider'
+import { useCart } from '../../context/providers/CartProvider'
+import { useWishlist } from '../../context/providers/WishlistProvider'
+import { deleteWishlistItem } from '../../utils/deleteWishlistItem'
+import { postCartItem } from '../../utils/postCartItem'
 
 function WishlistProductCard({ product }) {
-  return (
-    <div className="bg-regal-blue-dark rounded shadow-lg h-min">
-      <div className="h-full  flex flex-col">
-        <div className="badge-container">
-          {product.newArrival ? (
-            <span className="absolute font-bold card-badge badge bg-rose-500 text-white">
-              New
-            </span>
-          ) : (
-            ""
-          )}
+    const {
+        cartState: { cart },
+        cartDispatch,
+    } = useCart()
+    const {
+        wishlistState: { wishlist },
+        wishlistDispatch,
+    } = useWishlist()
 
-          <div className="p-5">
-            <img className="img-responsive" src={product.image} alt="" />
-          </div>
+    const {
+        authState: { encodedToken },
+    } = useAuth()
 
-          <button
-            style={{ backgroundColor: "rgba(255, 255, 255, 0.596)" }}
-            className="btn-close bg-blue-500 text-blue-500"
-          ></button>
-        </div>
-        <div className="p-5">
-          <h4 className="h4 font-normal text-lg subtitle1">
-            <div className="text-white">{product.title}</div>
-          </h4>
+    const isInCart = cart.find((item) => item._id === product._id)
 
-          <p className="body1 text-sm font-light mb-4 text-white">
-            {product.description}
-          </p>
+    console.log(isInCart)
 
-          <hr className="mb-3 text-regal-blue" />
+    deleteWishlistItem
 
-          <div className="flex justify-between">
-            <div className="product-price text-cyan-500">
-              <small className="line-through mr-2 text-white">
-                {product.actualPrice}
-              </small>
-              {product.discountedPrice}
+    return (
+        <div className="bg-regal-blue-dark rounded shadow-lg h-min">
+            <div className="h-full  flex flex-col">
+                <div className="badge-container">
+                    {product.offers.newArrival && (
+                        <span className="absolute font-bold card-badge badge bg-rose-500 text-white">
+                            New
+                        </span>
+                    )}
+
+                    <div className="p-5">
+                        <img
+                            className="img-responsive"
+                            src={product.img}
+                            alt={product.title}
+                        />
+                    </div>
+
+                    <button
+                        style={{
+                            backgroundColor: 'rgba(255, 255, 255, 0.596)',
+                        }}
+                        onClick={() =>
+                            deleteWishlistItem(
+                                product._id,
+                                encodedToken,
+                                wishlistDispatch
+                            )
+                        }
+                        className="btn-close bg-blue-500 text-blue-500"
+                    ></button>
+                </div>
+                <div className="p-5">
+                    <h4 className="h4 font-normal text-lg subtitle1">
+                        <div className="text-white">{product.title}</div>
+                    </h4>
+
+                    <p className="body1 text-sm font-light mb-4 text-white">
+                        {product.desc}
+                    </p>
+
+                    <hr className="mb-3 text-regal-blue" />
+
+                    <div className="flex justify-between">
+                        <div className="product-price text-cyan-500">
+                            <small className="line-through mr-2 text-white">
+                                {product.price}
+                            </small>
+                            {product.discountPrice}
+                        </div>
+
+                        <div className="flex">
+                            {isInCart ? (
+                                <Link
+                                    className="tooltip cursor-pointer"
+                                    to="/cart"
+                                >
+                                    <i className="fa fa-shopping-cart text-xl text-cyan-500"></i>
+                                    <span
+                                        style={{ textTransform: 'none' }}
+                                        className="tooltip-text shadow-lg"
+                                    >
+                                        go to cart
+                                    </span>
+                                </Link>
+                            ) : (
+                                <div
+                                    className="tooltip cursor-pointer"
+                                    onClick={() =>
+                                        postCartItem(
+                                            product,
+                                            encodedToken,
+                                            cartDispatch
+                                        )
+                                    }
+                                >
+                                    <i className="fa fa-shopping-cart text-xl text-grey-400 text-hover-cyan-500"></i>
+                                    <span
+                                        style={{ textTransform: 'none' }}
+                                        className="tooltip-text shadow-lg"
+                                    >
+                                        add to cart
+                                    </span>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="flex">
-              <div>
-                <i className="fa fa-heart mr-3 text-grey-400 text-hover-cyan-500"></i>
-              </div>
-              <div>
-                <i className="fa fa-shopping-cart text-grey-400 text-hover-cyan-500"></i>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default WishlistProductCard;
+export default WishlistProductCard
--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import WishlistProductCard from '../components/wishlist-page/WishlistProductCard'
-
-const product = {
-    newArrival: true,
-    image: 'https://picsum.photos/500',
-    title: 'Some Title',
-    description:
-        'lorem ipsum dolor sit lorem ipsum dolor sitlorem ipsum dolor sitlorem ipsum dolor sitlorem ipsum dolor sit',
-    actualPrice: 1000,
-    discountedPrice: 900,
-    qty: 1,
-}
+import { useWishlist } from '../context/providers/WishlistProvider'
 
 function Wishlist() {
+    const {
+        wishlistState: { wishlist },
+    } = useWishlist()
+
     return (
         <main
             style={{ top: '5rem', minHeight: '100vh' }}
@@ -21,7 +15,7 @@ function Wishlist() {
             <div className="spacer-3rem"></div>
             <div className="spacer-3rem"></div>
             <h3 className="text-4xl font-normal text-center text-white mb-2 underline">
-                My Wishlist (6)
+                My Wishlist ({wishlist.length})
             </h3>
 
             <div className="spacer-3rem"></div>
@@ -32,9 +26,9 @@ function Wishlist() {
                     style={{ gap: '4rem', maxWidth: '1200px', margin: 'auto' }}
                     className="grid grid-cols-3 p-10"
                 >
-                    <WishlistProductCard product={product} />
-                    <WishlistProductCard product={product} />
-                    <WishlistProductCard product={product} />
+                    {wishlist.map((product, i) => (
+                        <WishlistProductCard product={product} key={i} />
+                    ))}
                 </div>
             </div>
             <div className="spacer-3rem"></div>
