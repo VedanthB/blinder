ae844436a9ededa9c25c3df71db908c487f3dfbc
Vedanth Bora <vedanthbora@gmail.com>
2022-04-18 09:06:16 +0530

    [FIX]: bug fix

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react'
 import FiltersSidebar from '../components/shop-page/filters/FiltersSideBar'
 
 import ProductPageCard from '../components/shop-page/ProductPageCard'
+
 import SortProductsDropdown from '../components/shop-page/SortProductsDropdown'
+import FiltersSidebar from '../components/shop-page/filters/FiltersSideBar'
+
 import { useFilters } from '../context/providers/FilterProvider'
-import { useProducts } from '../context/providers/ProductProvider'
 
 function Shop() {
     const { sortedFilteredList } = useFilters()
@@ -14,7 +16,7 @@ function Shop() {
             style={{ top: '5rem', minHeight: '180vh' }}
             className="relative w-full justify-end flex"
         >
-            <FiltersSidebar />
+            <FiltersSideBar />
 
             <div className="products-display-container">
                 <div className="flex w-full justify-end">
