577959a679c3679cfe522bfc61b5e9f61facd77b
Vedanth Bora <vedanthbora@gmail.com>
2022-04-18 08:35:01 +0530

    [FIX]: bug fix

--- src/components/shop-page/filters/FIlterByBrands.js
+++ src/components/shop-page/filters/FIlterByBrands.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { filtersConstants } from "../../../context/constants/filtersConstants";
-import { useFilters } from "../../../context/providers/FilterProvider";
-import FilterListItem from "./FilterListItem";
+import React from 'react'
+import { filtersConstants } from '../../../context/constants/filtersConstants'
+import { useFilters } from '../../../context/providers/FilterProvider'
+import FilterListItem from './FilterListItem'
 
-const brands = ["Santa Cruz", "Vans", "Urban Monkey", "Thrasher"];
+const brands = ['Santa Cruz', 'Vans', 'Urban Monkey', 'Thrasher']
 
 function FilterByBrands() {
-  const { filtersState, filtersDispatch } = useFilters();
+    const { filtersState, filtersDispatch } = useFilters()
 
-  return (
-    <ul className="flex flex-col">
-      <li className="text-white pt-3 pb-3 text-lg">Brands</li>
-      {brands.map((listItem, i) => (
-        <FilterListItem
-          key={i}
-          value={filtersState.categories[listItem.toLowerCase()]}
-          onChange={() =>
-            filtersDispatch({
-              type: filtersConstants.FILTER_BY_BRAND,
-              payload: listItem.toLowerCase(),
-            })
-          }
-          text={listItem}
-        />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="flex flex-col">
+            <li className="text-white pt-3 pb-3 text-lg">Brands</li>
+            {brands.map((listItem, i) => (
+                <FilterListItem
+                    key={i}
+                    value={filtersState.categories[listItem.toLowerCase()]}
+                    onChange={() =>
+                        filtersDispatch({
+                            type: filtersConstants.FILTER_BY_BRAND,
+                            payload: listItem.toLowerCase(),
+                        })
+                    }
+                    text={listItem}
+                />
+            ))}
+        </ul>
+    )
 }
 
-export default FilterByBrands;
+export default FilterByBrands
